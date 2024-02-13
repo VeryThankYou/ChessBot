c3b6e030d90631ad7af788ed86df9b3d5af1b915
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2024-02-13 20:58:33 +0100

    Started defining legal moves

--- piece.py
+++ piece.py
@@ -1,10 +1,22 @@
 
 class Piece:
-    def __init__(self):
-        pass
+    def __init__(self, position, player):
+        self.position = position
+        self.player = player
+        self.hasmoved = False
+
+    
 
 class Pawn(Piece):
-    pass
+    def isMoveLegal(self, newPos):
+        if self.player.color == "w":
+            if newPos[1] == self.position[1] + 1 and newPos[0] == self.position[0]:
+                return True
+        else:
+            if newPos[1] == self.position[1] - 1 and newPos[0] == self.position[0]:
+                return True
+        return False
+
 
 class Knight(Piece):
     pass
@@ -20,3 +32,7 @@ class Queen(Piece):
 
 class King(Piece):
     pass
+
+if __name__=="__main__":
+    pawn = Pawn([3, 4])
+    print(pawn.position)
