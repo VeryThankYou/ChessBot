52e44da86c001cebcc0354135a4b8ef6ff1f3ab3
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2024-02-13 20:34:16 +0100

    classes made

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
 class Chess:
     def __init__(self):
-        self.grid = [[None for _ in range(8)] for i in range(8)]
+        self.grid = [[None for _ in range(8)] for _ in range(8)]
 
+
+if __name__=="__main__":
+    game = Chess()
+    print(game.grid)
---
+++ piece.py
@@ -0,0 +1,22 @@
+
+class Piece:
+    def __init__(self):
+        pass
+
+class Pawn(Piece):
+    pass
+
+class Knight(Piece):
+    pass
+
+class Bishop(Piece):
+    pass
+
+class Rook(Piece):
+    pass
+
+class Queen(Piece):
+    pass
+
+class King(Piece):
+    pass
