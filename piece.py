6e1cdf42cbe24f384bf3e8d1568f9e76d366e68e
buurdrengen <s203947@student.dtu.dk>
2024-02-16 14:00:16 +0100

    Pieces_movement

--- piece.py
+++ piece.py
@@ -16,22 +16,39 @@ class Pawn(Piece):
             if newPos[1] == self.position[1] - 1 and newPos[0] == self.position[0]:
                 return True
         return False
+    # Mangler del om har rykket eller ej 
 
 
 class Knight(Piece):
-    pass
+    def isMoveLegal(self,newPos):
+        if abs(newPos[0]-self.position[0])> 0 and abs(newPos[1]-self.position[1])> 0 and abs(newPos[0]-self.position[0]) + abs(newPos[1]-self.position[1]) == 3: 
+            return True 
+        return False 
+
 
 class Bishop(Piece):
-    pass
+    def isMoveLegal(self,newPos):
+        if abs(newPos[0]-self.position[0]) == abs(newPos[1]-self.position[1]):
+            return True
+        return False 
 
 class Rook(Piece):
-    pass
+    def isMoveLegal(self,newPos):
+        if abs(newPos[0]-self.position[0]) == 0 and abs(newPos[1]-self.position[1])>0 or abs(newPos[1]-self.position[1]) == 0 and abs(newPos[0]-self.position[0])>0:
+            # Jeg er lidt i tvivl om 'or' kan bruges her ordentligt 
+            return True 
+        # Mangler del om har rykket eller ej 
 
 class Queen(Piece):
-    pass
+    def isMoveLegal(self,newPos):
+        if abs(newPos[0]-self.position[0]) == abs(newPos[1]-self.position[1]) or abs(newPos[0]-self.position[0]) == 0 and abs(newPos[1]-self.position[1])>0 or abs(newPos[1]-self.position[1]) == 0 and abs(newPos[0]-self.position[0])>0:
+            # Er igen lidt i tvivl om 'or' er korrekt 
+            return True
 
 class King(Piece):
-    pass
+    def isMoveLegal(self,newPos):
+        if abs(newPos[0]-self.position[0]) == 1 and abs(newPos[1]-self.position[1]) == 1 or abs(newPos[0]-self.position[0]) == 0 and abs(newPos[1]-self.position[1]) == 1 or abs(newPos[0]-self.position[0]) == 1 and abs(newPos[1]-self.position[1]) == 0:
+            return True 
 
 if __name__=="__main__":
     pawn = Pawn([3, 4])
