c63321d2d3f337a71f56cb253a6527674b5b71a8
buurdrengen <s203947@student.dtu.dk>
2024-02-26 11:01:25 +0100

    pieces

--- piece.py
+++ piece.py
@@ -7,24 +7,45 @@ class Piece:
         self.hasmoved = False
 
         # Hvis vi laver init med value: 
-        # self.value_sign = 1 if color == "w" else -1 
-        # self.value = value*value_sign
+        self.value_sign = 1 if self.player.color == "w" else -1 
+        #self.value = value*self.value_sign
 
     
 
 class Pawn(Piece):
+    def __init__ (self, position, player):
+        super(Pawn,self).__init__(self, position, player)
+        self.value = 1*self.value_sign
+
     def isMoveLegal(self, newPos):
         if self.player.color == "w":
+            # if self.position[1] == 1:#6?
+            if self.hasmoved == False:
+                if newPos[1] == self.position[1] + 2 and newPos[0] == self.position[0]:
+                    # self.hasmoved = True
+                    return True 
+                 
+
             if newPos[1] == self.position[1] + 1 and newPos[0] == self.position[0]:
                 return True
+                
         else:
+            if self.hasmoved == False:
+                if newPos[1] == self.position[1] - 2 and newPos[0] == self.position[0]:
+                    return True 
+            # if self.position[1] == 6:#1?
+            #     newPos[1] == self.position[1] - 2 and newPos[0] == self.position[0] 
             if newPos[1] == self.position[1] - 1 and newPos[0] == self.position[0]:
                 return True
+        
         return False
     # Mangler del om har rykket eller ej 
 
 
 class Knight(Piece):
+    def __init__ (self, position, player):
+        super(Knight,self).__init__(self,position,player)
+        self.value = 3*self.value_sign
     def isMoveLegal(self,newPos):
         if abs(newPos[0]-self.position[0])> 0 and abs(newPos[1]-self.position[1])> 0 and abs(newPos[0]-self.position[0]) + abs(newPos[1]-self.position[1]) == 3: 
             return True 
@@ -32,12 +53,18 @@ class Knight(Piece):
 
 
 class Bishop(Piece):
+    def __init__ (self, position, player):
+        super(Bishop,self).__init__(self, position, player)
+        self.value = 3*self.value_sign
     def isMoveLegal(self,newPos):
         if abs(newPos[0]-self.position[0]) == abs(newPos[1]-self.position[1]):
             return True
         return False 
 
 class Rook(Piece):
+    def __init__ (self, position, player):
+        super(Rook,self).__init__(self, position, player)
+        self.value = 5*self.value_sign
     def isMoveLegal(self,newPos):
         if (abs(newPos[0]-self.position[0]) == 0 and abs(newPos[1]-self.position[1])>0) or (abs(newPos[1]-self.position[1]) == 0 and abs(newPos[0]-self.position[0])>0):
             # Jeg er lidt i tvivl om 'or' kan bruges her ordentligt 
@@ -46,6 +73,9 @@ class Rook(Piece):
         return False
 
 class Queen(Piece):
+    def __init__ (self, position, player):
+        super(Queen,self).__init__(self, position, player)
+        self.value = 9*self.value_sign
     def isMoveLegal(self,newPos):
         if (abs(newPos[0]-self.position[0]) == abs(newPos[1]-self.position[1])) or (abs(newPos[0]-self.position[0]) == 0 and abs(newPos[1]-self.position[1])>0) or (abs(newPos[1]-self.position[1]) == 0 and abs(newPos[0]-self.position[0])>0):
             # Er igen lidt i tvivl om 'or' er korrekt 
@@ -53,10 +83,15 @@ class Queen(Piece):
         return False
 
 class King(Piece):
+    def __init__ (self, position, player):
+        super(King,self).__init__(self, position, player)
+        self.value = 1000*self.value_sign
     def isMoveLegal(self,newPos):
         if abs(newPos[0]-self.position[0]) <= 1 and abs(newPos[1]-self.position[1]) <= 1:
             return True 
         return False
+    # Mangler del om har rykket eller ej 
+
 
 if __name__=="__main__":
     pawn = Pawn([3, 4])
